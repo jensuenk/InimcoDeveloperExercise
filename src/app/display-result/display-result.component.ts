f532a9e5ff53ffc34085813c0634daa17b6e1b3e
Jens Uenk <jens.uenk@sdp.be>
2024-06-24 22:43:15 +0200

    Initialised client project

---
+++ src/app/display-result/display-result.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './display-result.component.html',
+  styleUrl: './display-result.component.scss'
+})
+export class DisplayResultComponent {
+
+}

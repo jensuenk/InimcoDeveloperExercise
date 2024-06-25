877ca689b9cdc59c9fc3cb5d1bf94b479f9731ed
Jens Uenk <jens.uenk@sdp.be>
2024-06-26 01:13:30 +0200

    Added unit test

--- InimcoDeveloperExerciseClient/src/app/modules/user-input/user-input.component.spec.ts
+++ InimcoDeveloperExerciseClient/src/app/modules/user-input/user-input.component.spec.ts
@@ -1,23 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// user-input.component.spec.ts
 
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { UserInputComponent } from './user-input.component';
+import { UserService } from '../../providers/services/user.service';
 
 describe('UserInputComponent', () => {
   let component: UserInputComponent;
   let fixture: ComponentFixture<UserInputComponent>;
+  let snackBarOpenSpy: jasmine.Spy;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UserInputComponent]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    const userServiceMock = jasmine.createSpyObj('UserService', ['submitUserProfile']);
+
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        MatCardModule,
+        MatInputModule,
+        MatSelectModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSnackBarModule
+      ],
+      providers: [
+        { provide: UserService, useValue: userServiceMock },
+        MatSnackBar
+      ]
+    }).compileComponents();
+
+    snackBarOpenSpy = spyOn(TestBed.inject(MatSnackBar), 'open');
 
     fixture = TestBed.createComponent(UserInputComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  }));
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize form with empty values', () => {
+    expect(component.userProfileForm.value).toEqual({
+      firstName: '',
+      lastName: '',
+      socialSkills: [],
+      socialAccounts: []
+    });
+  });
+
+  it('should add and remove social skills', () => {
+    component.addSocialSkill();
+    expect(component.socialSkills.length).toEqual(1);
+
+    component.removeSocialSkill(0);
+    expect(component.socialSkills.length).toEqual(0);
+  });
+
+  it('should add and remove social accounts', () => {
+    component.addSocialAccount();
+    expect(component.socialAccounts.length).toEqual(1);
+
+    component.removeSocialAccount(0);
+    expect(component.socialAccounts.length).toEqual(0);
+  });
+
 });
+
